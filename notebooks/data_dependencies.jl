ec8f3d94cef4a45d76834aea033aea17b0686738
Robin Deits <robin.deits@gmail.com>
2019-05-21 13:46:43 -0400

    Try out datadeps

---
+++ notebooks/data_dependencies.jl
@@ -0,0 +1,9 @@
+using DataDeps
+
+register(DataDep(
+    "bouman_edge_cameras_example_videos",
+    "This dataset contains example videos provided with the original paper \"Turning Corners into Cameras\" by Bouman et al.",
+    "https://people.csail.mit.edu/klbouman/pw/projects/cornercam/example_videos.zip",
+    [],
+    post_fetch_method=unpack
+))
