f1eafd894cd053d8fb9e6773411d15cfb6c91f96
Robin Deits <robin.deits@gmail.com>
2019-05-21 17:37:04 -0400

    try to fix datadeps usage

--- notebooks/data_dependencies.jl
+++ notebooks/data_dependencies.jl
@@ -4,6 +4,5 @@ register(DataDep(
     "bouman_edge_cameras_data",
     "This dataset contains example videos provided with the original paper \"Turning Corners into Cameras\" by Bouman et al.",
     "https://people.csail.mit.edu/klbouman/pw/projects/cornercam/example_videos.zip",
-    [],
     post_fetch_method=unpack
 ))
